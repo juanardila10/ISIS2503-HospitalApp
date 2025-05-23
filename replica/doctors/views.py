7820b3c6f5608880d2267408b583d74c1dc1fe6a
juanardila10 <js.ardilal1@uniades.edu.co>
2025-05-23 02:17:59 -0500

    YA

--- replica/doctors/views.py
+++ replica/doctors/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
-from .forms import VariableForm
 from .logic.doctor_logic import get_doctors, create_doctor
 
 def doctor_list(request):
